08f5748120a02152cd22522aab543bd86a0ba521
Utkarsh Kashyap <utkarsh.k05@gmail.com>
2017-04-09 03:10:45 +0530

    database init

--- ahack.sql
+++ ahack.sql
@@ -19,6 +19,10 @@
 -- Table structure for table `messages`
 --
 
+drop database ahack;
+create database ahack;
+use ahack;
+
 DROP TABLE IF EXISTS `messages`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
